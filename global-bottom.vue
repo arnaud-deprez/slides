8951423cb58e87a76e79144c35297422ed998da8
Arnaud Deprez <arnaudeprez@gmail.com>
2025-03-24 17:59:51 +0100

    chore: bump versions to latest

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { useNav } from '@slidev/client'
+const { currentPage, total } = useNav()
 </script>
 
 <template>
   <footer class="absolute bottom-0 px-14 pb-4 pt-8 w-full flex">
-    <div class="ml-auto text-xs text-gray-500">{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</div>
+    <div class="ml-auto text-xs text-gray-500">{{ currentPage }} / {{ total }}</div>
   </footer>
 </template>
